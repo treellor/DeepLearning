0d8dc1c38f706d0c8ad92cbed4bb5b470685cf13
treellor <gq620@163.com>
2022-12-24 12:20:05 +0800

    SRCNN修改

--- SR/SRCNN_train.py
+++ SR/SRCNN_train.py
@@ -3,7 +3,7 @@
     Version		:  v1.0
     Date		:  2021.12.19
     Description	:
-            网络训练
+            SRGAN 仿真模型
     Others		:  //其他内容说明
     History		:
      1.Date:
@@ -15,23 +15,26 @@
 import os
 import numpy as np
 import math
-
-from PIL import Image
-from matplotlib import pyplot as plt
-
+import argparse
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+from PIL import Image
+from matplotlib import pyplot as plt
 from tqdm import tqdm
-
 from torch.utils.data import DataLoader
-from torchvision.transforms import Compose, RandomCrop, ToTensor, Resize, ToPILImage
 
-from data_read import DatasetFromFolder
+from data_read import ImageDatasetResize
 from SRCNN_model import SRCNN
 
+
 def psnr(label, outputs, max_val=1.0):
+    """
+        per_image_psnr = 10 * torch.log10(10 / per_image_mse_loss)
+        tensor_average_psnr = torch.mean(per_image_psnr).to(device)
+        loss = tensor_average_psnr
+    """
     label = label.cpu().detach().numpy()
     outputs = outputs.cpu().detach().numpy()
     image_diff = outputs - label
@@ -43,21 +46,7 @@ def psnr(label, outputs, max_val=1.0):
         return PNSR
 
 
-def get_image_hl(path):
-    data_to_tensor = Compose([ToTensor()])
-    img_hr = Image.open(path)
-    w, h = img_hr.size
-
-    img_lr = img_hr.resize((int(w / 3), int(h / 3)))
-    img_lr = img_lr.resize((w, h), Image.Resampling.BICUBIC)
-    img_hr = data_to_tensor(img_hr)
-    img_lr = data_to_tensor(img_lr)
-    img_hr = img_hr.unsqueeze(0)
-    img_lr = img_lr.unsqueeze(0)
-    return img_hr, img_lr
-
-
-def show_example_image(image_hr, image_lr, image_new, save_folder):
+def save_example_image(image_hr, image_lr, image_new, save_folder, is_show=False):
     _, _, h, w = image_hr.size()
     torchvision.utils.save_image(image_lr, os.path.join(save_folder, 'LR.png'))
     torchvision.utils.save_image(image_hr, os.path.join(save_folder, 'HR.png'))
@@ -72,34 +61,36 @@ def show_example_image(image_hr, image_lr, image_new, save_folder):
     dst.paste(im2, (w, 0))
     dst.paste(im3, (w * 2, 0))
     dst.save(os.path.join(save_folder, 'image.png'))
-    img = Image.open(os.path.join(save_folder, 'image.png'))
-    plt.imshow(img)
-    plt.title('new Image')
-    plt.show()
+    if is_show:
+        img = Image.open(os.path.join(save_folder, 'image.png'))
+        plt.imshow(img)
+        plt.title('new Image')
+        plt.show()
 
 
-def train(imageForder, n_epochs=31, load_pretrained_model=False):
-    folder_save_image = r".\images\SRCNN"
-    folder_model = r".\models\SRCNN"
-
+def train(opt):
+    folder_save_image = opt.folder_save_image
+    folder_save_model = opt.folder_save_model
     os.makedirs(folder_save_image, exist_ok=True)
-    os.makedirs(folder_model, exist_ok=True)
+    os.makedirs(folder_save_model, exist_ok=True)
 
-    #imageForder = '../data/T91/'
-    dataset = DatasetFromFolder(imageForder, Compose([RandomCrop([60, 60]), ToTensor()]))
-    # dataset  = DatasetHighLow(r"D:\project\Pycharm\DeepLearning\data\coco125\high",  r"D:\project\Pycharm\DeepLearning\data\coco125\low")
+    data_folder = opt.folder_data
+    batch_size = opt.batch_size
+    img_h = opt.crop_img_h
+    img_w = opt.crop_img_w
+    dataset = ImageDatasetResize(data_folder, [img_h, img_w], is_same_shape=True)
+    # dataset = ImageDatasetCrop(data_folder, [256,128], is_same_shape=True)
 
-    train_set, val_set = torch.utils.data.random_split(dataset, [dataset.__len__() - 16, 16])
-    val_loader = DataLoader(dataset=val_set, num_workers=0, batch_size=8, shuffle=True)
-    train_loader = DataLoader(dataset=train_set, num_workers=0, batch_size=16, shuffle=True)
+    data_len = dataset.__len__()
+    val_data_len = int(data_len * 0.25)
+    train_set, val_set = torch.utils.data.random_split(dataset, [data_len - val_data_len, val_data_len])
+    train_loader = DataLoader(dataset=train_set, num_workers=0, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(dataset=val_set, num_workers=0, batch_size=int(batch_size // 2), shuffle=True)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     srcnn = SRCNN()
-
-    load_pretrained_models = False
-    # Load pretrained models
-    if load_pretrained_models:
-        srcnn.load_state_dict(torch.load(os.path.join(folder_model, "last_ckpt.pth")))
+    if opt.load_models:
+        srcnn.load_state_dict(torch.load(opt.load_models_path))
 
     if torch.cuda.device_count() > 1:
         srcnn = nn.DataParallel(srcnn)
@@ -109,84 +100,86 @@ def train(imageForder, n_epochs=31, load_pretrained_model=False):
     optimizer = optim.Adam(srcnn.parameters())
     criterion = nn.MSELoss().to(device)
 
-    train_loss, val_loss = [], []
-    train_psnr, val_psnr = [], []
+    #             per_image_mse_loss = F.mse_loss(HR, newHR, reduction='none')
+
+    train_loss_all, val_loss_all = [], []
+    train_psnr_all, val_psnr_all = [], []
 
     best_loss = 1e100
-    best_psnr = 0  # 记录最高
-
-    show_example = True
 
     # 读取显示图像
-    show_image_hr, show_image_lr = get_image_hl(imageForder + 't1.png')
-    show_image_hr = show_image_hr.to(device)
-    show_image_lr = show_image_lr.to(device)
-
+    show_example = opt.show_example
+    show_image_hr = None
+    show_image_lr = None
+    n_epochs = opt.epochs
     for epoch in tqdm(range(n_epochs)):
-        # print("Epoch:{}".format(epoch))
-        running_loss = 0.0
-        running_psnr = 0.0
+        train_loss = 0.0
+        train_psnr = 0.0
         srcnn.train()
-        is_shown = False
-        for HR, LR in train_loader:
-            HR = HR.to(device)
-            LR = LR.to(device)
+        for images_hl in train_loader:
+            LR = images_hl["lr"].to(device)
+            HR = images_hl["hr"].to(device)
+
+            if show_example and epoch == 0 and show_image_hr is None:
+                show_image_hr = HR
+                show_image_lr = LR
 
             newHR = srcnn(LR)
             srcnn.zero_grad()
-            optimizer.zero_grad()  # 可能不需要
-            loss = criterion(HR, newHR)
-            #             per_image_mse_loss = F.mse_loss(HR, newHR, reduction='none')
-            #             per_image_psnr = 10 * torch.log10(10 / per_image_mse_loss)
-            #             tensor_average_psnr = torch.mean(per_image_psnr).to(device)
-            #             loss = tensor_average_psnr
-            loss.backward(retain_graph=True)
+            optimizer.zero_grad()
+            train_loss_content = criterion(HR, newHR)
+            train_loss_content.backward(retain_graph=True)
             optimizer.step()
-            running_loss += loss.item()
-            batch_psnr = psnr(HR, newHR)
-            running_psnr += batch_psnr
+            train_loss += train_loss_content.item()
+            train_psnr += psnr(HR, newHR)
 
-        final_loss = running_loss / len(train_loader.dataset)
-        final_psnr = running_psnr / int(len(train_set) / train_loader.batch_size)
-        train_loss.append(final_loss)
-        train_psnr.append(final_psnr)
+        final_loss = train_loss / len(train_loader.dataset)
+        final_psnr = train_psnr / int(len(train_set) / train_loader.batch_size)
+        train_loss_all.append(final_loss)
+        train_psnr_all.append(final_psnr)
 
         # 验证
         srcnn.eval()
-        val_loss_temp = 0.0
-        val_psnr_temp = 0.0
+        val_loss = 0.0
+        val_psnr = 0.0
         with torch.no_grad():
             #  for _, data in tqdm(enumerate(dataloader), total=int(len(val_set) / dataloader.batch_size)):
-            for _, data in enumerate(val_loader):
-                image_data = data[0].to(device)
-                label = data[1].to(device)
+            for idx, datas_hl in enumerate(val_loader):
+                image_l = datas_hl["lr"].to(device)
+                image_h = datas_hl["hr"].to(device)
 
-                outputs = srcnn(image_data)
-                loss_temp = criterion(outputs, label)
-                val_loss_temp += loss_temp.item()
-                batch_psnr_temp = psnr(label, outputs)
-                val_psnr_temp += batch_psnr_temp
+                # image_l = data[0].to(device)
+                # image_h = data[1].to(device)
+                image_gen = srcnn(image_l)
+                val_loss_content = criterion(image_gen, image_h)
+                val_loss += val_loss_content.item()
+                val_psnr += psnr(image_h, image_gen)
+                # 显示训练结果
+                if idx == 0:
+                    if epoch == n_epochs - 1:
+                        show_image_gen = srcnn(show_image_lr)
+                        save_example_image(show_image_hr, show_image_lr, show_image_gen, folder_save_image, True)
+                    else:
+                        if show_example:
+                            if epoch % 10 == 0:
+                                show_image_gen = srcnn(show_image_lr)
+                                save_example_image(show_image_hr, show_image_lr, show_image_gen, folder_save_image,
+                                                   True)
 
-        val_epoch_loss = val_loss_temp / len(val_loader.dataset)
-        val_epoch_psnr = val_psnr_temp / int(len(val_set) / val_loader.batch_size)
-        val_loss.append(val_epoch_loss)
-        val_psnr.append(val_epoch_psnr)
+        val_epoch_loss = val_loss / len(val_loader.dataset)
+        val_epoch_psnr = val_psnr / int(len(val_set) / val_loader.batch_size)
+        val_loss_all.append(val_epoch_loss)
+        val_psnr_all.append(val_epoch_psnr)
 
         # 保存最新的参数和损失最小的参数
-        torch.save(srcnn.state_dict(), os.path.join(folder_model, 'last_ckpt.pth'))
+        torch.save(srcnn.state_dict(), os.path.join(folder_save_model, 'last_ckpt.pth'))
         if best_loss > final_loss:
-            torch.save(srcnn.state_dict(), os.path.join(folder_model, 'best_ckpt.pth'))
+            torch.save(srcnn.state_dict(), os.path.join(folder_save_model, 'best_ckpt.pth'))
             best_loss = final_loss
 
-        # 显示训练结果
-        if ((epoch % 15 == 0) or epoch == n_epochs - 1) and show_example == True:
-            srcnn.eval()
-            show_image_new = srcnn(show_image_lr)
-            show_example_image(show_image_hr, show_image_lr, show_image_new, folder_save_image)
-
     plt.figure(figsize=(10, 7))
-    plt.plot(train_loss, color='orange', label='train loss')
-    plt.plot(val_loss, color='red', label='validation loss')
+    plt.plot(train_loss_all, color='orange', label='train loss')
+    plt.plot(val_loss_all, color='red', label='validation loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
@@ -194,8 +187,8 @@ def train(imageForder, n_epochs=31, load_pretrained_model=False):
     plt.show()
 
     plt.figure(figsize=(10, 7))
-    plt.plot(train_psnr, color='green', label='train PSNR dB')
-    plt.plot(val_psnr, color='blue', label='validation PSNR dB')
+    plt.plot(train_psnr_all, color='green', label='train PSNR dB')
+    plt.plot(val_psnr_all, color='blue', label='validation PSNR dB')
     plt.xlabel('Epochs')
     plt.ylabel('PSNR (dB)')
     plt.legend()
@@ -203,6 +196,24 @@ def train(imageForder, n_epochs=31, load_pretrained_model=False):
     plt.show()
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="You should add those parameter!")
+    parser.add_argument('--folder_data', type=str, default='data/coco128.yaml', help='dataset path')
+    parser.add_argument('--crop_img_w', type=int, default=64, help='randomly cropped image width')
+    parser.add_argument('--crop_img_h', type=int, default=64, help='randomly cropped image height')
+    parser.add_argument('--folder_save_image', type=str, default=r".\images\SRCNN", help='image save path')
+    parser.add_argument('--folder_save_model', type=str, default=r".\models\SRCNN", help='model save path')
+    parser.add_argument('--load_models', type=bool, default=False, help='load pretrained model weight')
+    parser.add_argument('--load_models_path', type=str, default=r".\models\SRCNN\last_ckpt.pth", help='load model path')
+    parser.add_argument('--epochs', type=int, default=100, help='total training epochs')
+    parser.add_argument('--batch_size', type=int, default=16, help='total batch size for all GPUs')
+    parser.add_argument('--show_example', type=bool, default=True, help='show a validation example')
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == '__main__':
-    imageForder = '../data/T91/'
-    train(imageForder)
+    para = parse_args()
+    para.folder_data = '../data/T91/'
+    para.load_models = False
+    train(para)
--- SR/data_read.py
+++ SR/data_read.py
@@ -1,7 +1,7 @@
 """
     Author		:  Treellor
     Version		:  v1.0
-    Date		:  2021.12.19
+    Date		:  2022.12.19
     Description	:
             创建数据集程序
     Others		:  //其他内容说明
@@ -12,10 +12,9 @@
      2.…………
 """
 import os
-import numpy as np
+# import numpy as np
 from torch.utils.data import Dataset
 from PIL import Image
-from torchvision.transforms import Compose, ToPILImage, ToTensor
 import torchvision.transforms as tfs
 
 
@@ -23,32 +22,44 @@ def is_image_file(filename):
     return any(filename.endswith(extension) for extension in ['.bmp', '.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
 
 
-class DatasetFromFolder(Dataset):
-    def __init__(self, path, transforms=None, down_sampling=4):
-        super(DatasetFromFolder, self).__init__()
-        self.down_sampling = down_sampling
-        if transforms is None:
-            self.transforms = Compose([ToTensor()])
-        else:
-            t = transforms.transforms
-            is_exist = False
-            for v in t:
-                if isinstance(v, ToTensor):
-                    is_exist = True
-                    break
-            if is_exist:
-                self.transforms = transforms
-            else:
-                self.transforms = Compose([*t, ToTensor()])
+class ImageDatasetResize(Dataset):
+    """
+    将原有数据集缩放到固定大小
+    """
 
-        self.data_transform_PIL = Compose([ToPILImage()])
-        self.data_transform_Tensor = Compose([ToTensor()])
+    def __init__(self, path, resize_shape_hw=[128, 128], is_same_shape=False, down_sampling=4, is_Normalize=False,
+                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_count=None):
+
+        super(ImageDatasetResize, self).__init__()
+
+        hr_height, hr_width = resize_shape_hw
+
+        hr_trans = [tfs.Resize((hr_height, hr_height), tfs.InterpolationMode.BICUBIC), tfs.ToTensor()]
+        if is_Normalize:
+            hr_trans.append(tfs.Normalize(mean, std))
+        self.hr_transforms = tfs.Compose(hr_trans)
+
+        lr_trans = [tfs.Resize((hr_height // down_sampling, hr_width // down_sampling), tfs.InterpolationMode.BICUBIC),
+                    tfs.ToTensor()]
+        if is_Normalize:
+            lr_trans.append(tfs.Normalize(mean, std))
+        self.lr_transforms = tfs.Compose(lr_trans)
+
+        self.is_same_shape = is_same_shape
+        if is_same_shape:
+            self.data_transform_PIL = tfs.Compose([tfs.ToPILImage()])
+
         self.filePaths = []
         folders = os.listdir(path)
+        count = 0
         for f in folders:
-            fp = path + f
+            fp = os.path.join(path, f)
             if is_image_file(fp):
+                if max_count is not None:
+                    if count >= max_count:
+                        break
                 self.filePaths.append(path + f)
+                count = count + 1
 
     def __len__(self):
         return len(self.filePaths)
@@ -57,27 +68,86 @@ class DatasetFromFolder(Dataset):
         img = Image.open(self.filePaths[item])
         if img.mode != 'RGB':
             raise ValueError("Image:{} isn't RGB mode.".format(self.filePaths[item]))
-
-        # img, _cb, _cr = img.convert('YCbCr').split()
-        if self.transforms is not None:
-            img = self.transforms(img)
-
-        result_image = img
-        _, h, w = result_image.size()
-
-        resize_image = self.data_transform_PIL(result_image)
-        resize_image = resize_image.resize((int(w / self.down_sampling), int(h / self.down_sampling)))
-        resize_image = resize_image.resize((w, h), Image.BICUBIC)
-        resize_image = self.data_transform_Tensor(resize_image)
-
-        return result_image, resize_image
+        # img, _cb, _cr = img.convert('YCbCr').split() 图像转换
+        img_lr = self.lr_transforms(img)
+        img_hr = self.hr_transforms(img)
+        if self.is_same_shape:
+            img_lr = self.data_transform_PIL(img_lr)
+            img_lr = self.hr_transforms(img_lr)
+        return {"lr": img_lr, "hr": img_hr}
 
 
-class DatasetHighLow(Dataset):
-    def __init__(self, path_folder_high, path_folder_low):
-        super(DatasetHighLow, self).__init__()
+class ImageDatasetCrop(Dataset):
+    """
+    将原有数据集裁剪到固定大小
+    """
 
-        self.data_transform_Tensor = Compose([ToTensor()])
+    def __init__(self, path, crop_shape_hw=[128, 128], is_same_shape=False, down_sampling=4, is_Normalize=False,
+                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_count=None):
+
+        super(ImageDatasetCrop, self).__init__()
+
+        self.is_save_shape = is_same_shape
+        hr_height, hr_width = crop_shape_hw
+        self.crop_transforms = tfs.Compose([tfs.RandomCrop((hr_height, hr_height), Image.BICUBIC)])
+        self.resize_transforms_down = tfs.Compose([tfs.Resize((hr_height // down_sampling, hr_width // down_sampling),
+                                                              tfs.InterpolationMode.BICUBIC)])
+
+        hr_trans = [tfs.ToTensor()]
+        if is_Normalize:
+            hr_trans.append(tfs.Normalize(mean, std))
+        self.hr_transforms = tfs.Compose(hr_trans)
+
+        lr_trans = [tfs.ToTensor()]
+        if is_same_shape:
+            lr_trans.append(tfs.Resize((hr_height, hr_width), tfs.InterpolationMode.BICUBIC))
+        if is_Normalize:
+            lr_trans.append(tfs.Normalize(mean, std))
+        self.lr_transforms = tfs.Compose(lr_trans)
+
+        self.filePaths = []
+        folders = os.listdir(path)
+        count = 0
+        for f in folders:
+            fp = os.path.join(path, f)
+
+            img = Image.open(fp)
+            if img.mode != 'RGB':
+                continue
+            w, h = img.size
+            if w < hr_width or h < hr_height:
+                continue
+            if max_count is not None:
+                if count >= max_count:
+                    break
+            self.filePaths.append(path + f)
+            count = count + 1
+
+    def __len__(self):
+        return len(self.filePaths)
+
+    def __getitem__(self, item):
+        img = Image.open(self.filePaths[item])
+        if img.mode != 'RGB':
+            raise ValueError("Image:{} isn't RGB mode.".format(self.filePaths[item]))
+        # img, _cb, _cr = img.convert('YCbCr').split() 图像转换
+
+        crop_image = self.crop_transforms(img)
+        image_down = self.resize_transforms_down(crop_image)
+        img_hr = self.hr_transforms(crop_image)
+        img_lr = self.lr_transforms(image_down)
+        return {"lr": img_lr, "hr": img_hr}
+
+
+class ImageDatasetHighLow(Dataset):
+    def __init__(self, path_folder_high, path_folder_low,
+                 is_Normalize=False, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_count=None):
+        super(ImageDatasetHighLow, self).__init__()
+
+        self.transforms_Tensor = tfs.Compose([tfs.ToTensor()])
+        self.is_Normalize = is_Normalize
+        if self.is_Normalize:
+            self.transforms_Normalize = tfs.Compose([tfs.Normalize(mean, std)])
         self.filePathsHigh = []
         self.filePathsLow = []
         images_high = os.listdir(path_folder_high)
@@ -94,105 +164,28 @@ class DatasetHighLow(Dataset):
     def __getitem__(self, item):
         img1 = Image.open(self.filePathsHigh[item])
         img2 = Image.open(self.filePathsLow[item])
-        image_high = self.data_transform_Tensor(img1)
-        image_low = self.data_transform_Tensor(img2)
+        image_high = self.transforms_Tensor(img1)
+        image_low = self.transforms_Tensor(img2)
+        if self.is_Normalize:
+            image_high = self.transforms_Normalize(image_high)
+            image_low = self.transforms_Normalize(image_low)
         return {"lr": image_low, "hr": image_high}
-        #return image_high, image_low
 
 
-class SRGANDataset(Dataset):
-    def __init__(self, data_path, ty="train"):
-        self.dataset = []
-        self.path = data_path
-        self.ty = ty
-        f = open(os.path.join(data_path, "{}.txt".format(ty)))
-        self.dataset.extend(f.readlines())
-        f.close()
-        self.tfs = tfs.Compose([
-            tfs.ToTensor(),
-            tfs.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
+# import torch
+# from torch.utils.data import DataLoader
 
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, index):
-        img_name = self.dataset[index].strip()
-        img = Image.open(os.path.join(self.path, self.ty, "img", img_name))
-        label = Image.open(os.path.join(self.path, self.ty, "label", img_name))
-        img = self.tfs(img)
-        label = self.tfs(label)
-        return img, label
-
-
-mean = np.array([0.485, 0.456, 0.406])
-std = np.array([0.229, 0.224, 0.225])
-
-
-class ImageDataset(Dataset):
-    def __init__(self, files, hr_shape):
-        hr_height, hr_width = hr_shape
-        # Transforms for low resolution images and high resolution images
-        self.lr_transform = tfs.Compose(
-            [
-                tfs.Resize((hr_height // 4, hr_height // 4), Image.BICUBIC),
-                tfs.ToTensor(),
-                tfs.Normalize(mean, std),
-            ]
-        )
-        self.hr_transform = tfs.Compose(
-            [
-                tfs.Resize((hr_height, hr_height), Image.BICUBIC),
-                tfs.ToTensor(),
-                tfs.Normalize(mean, std),
-            ]
-        )
-        self.files = files
-
-    def __getitem__(self, index):
-        img = Image.open(self.files[index % len(self.files)])
-        img_lr = self.lr_transform(img)
-        img_hr = self.hr_transform(img)
-
-        return {"lr": img_lr, "hr": img_hr}
-
-    def __len__(self):
-        return len(self.files)
-
-import torch
-from torch.utils.data import DataLoader
-from SRGAN_models import GeneratorResNet, DiscriminatorNet, FeatureExtractor
-from torch.autograd import Variable
 if __name__ == '__main__':
-    datas = DatasetHighLow(r"D:\project\Pycharm\DeepLearning\data\coco125\high",
-                           r"D:\project\Pycharm\DeepLearning\data\coco125\low")
-    '''
-        e = SRGANDataset(r"T:\\srgan")
-    a, b = e[0]
-    print(a)
-    '''
-
-    dataset = DatasetHighLow(r"D:\project\Pycharm\DeepLearning\data\coco125\high",
-                             r"D:\project\Pycharm\DeepLearning\data\coco125\low")
+    """
+    dataset = ImageDatasetHighLow(r"D:\project\Pycharm\DeepLearning\data\coco125\high",
+                             r"D:\project\Pycharm\DeepLearning\data\coco125\low", is_Normalize=True)
     train_set, val_set = torch.utils.data.random_split(dataset, [dataset.__len__() - 32, 32])
     test_dataloader = DataLoader(dataset=val_set, num_workers=0, batch_size=16, shuffle=True)
     train_dataloader = DataLoader(dataset=train_set, num_workers=0, batch_size=16, shuffle=True)
 
-    # Initialize generator and discriminator
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    cuda = torch.cuda.is_available()
-    Tensor = torch.cuda.FloatTensor if cuda else torch.Tensor
 
-    discriminator = DiscriminatorNet().to(device)
-    for  imgs in train_dataloader:
-
-            # Configure model input
-            imgs_lr = Variable(imgs["lr"].type(Tensor))
-            imgs_hr = Variable(imgs["hr"].type(Tensor))
-            # Adversarial ground truths
-            #A  = discriminator(imgs_hr)
-            #print(A)
-
-           # valid = Variable(Tensor(np.ones((imgs_lr.size(0), 1))), requires_grad=False)
-           # fake = Variable(Tensor(np.zeros((imgs_lr.size(0), *discriminator.output_shape))), requires_grad=False)
-
+    for imgs in train_dataloader:
+        # Configure model input
+        imgs_lr = imgs["lr"]
+        imgs_hr = imgs["hr"]
+    """
