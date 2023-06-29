8d430b379050f5484689e637cd9b04c686334754
treellor <gq620@163.com>
2023-06-29 17:52:41 +0800

    20230629

--- utils/common.py
+++ utils/common.py
@@ -69,7 +69,19 @@ class EMA():
     #         current_params.data = ema_params.data.clone()
 
 
-
+# class EMA():
+#     def __init__(self, decay):
+#         self.decay = decay
+#
+#     def update_average(self, old, new):
+#         if old is None:
+#             return new
+#         return old * self.decay + (1 - self.decay) * new
+#
+#     def update_model_average(self, ema_model, current_model):
+#         for current_params, ema_params in zip(current_model.parameters(), ema_model.parameters()):
+#             old, new = ema_params.data, current_params.data
+#             ema_params.data = self.update_average(old, new)
 
 def generate_cosine_schedule(T, s=0.008):
     def f(t, T):
